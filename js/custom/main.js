16f2093f546872d8bd9ca519a9fe07b45c9d0ebf
SwannSG <swannsg@gmail.com>
2018-08-04 09:26:15 +0200

    new

--- js/custom/main.js
+++ js/custom/main.js
@@ -1,16 +1,28 @@
+// only one namespace "ws" for our code
 if ('ws' in window) {
     console.log('Namespace "ws" already exists');
 }
 else {
     window.ws = {};
 }
+// end only one namespace "ws" for our code
 
-// CTN_CENTER =  [-33.9249, 18.4241];
-// CONFIG (add all provinces below)
+
+
+// CONFIG (add all provinces below) ************************
 ws.CONFIG = {
+    BIN_SIZE: 2000,
+    COLORS: {
+        BIN_1: '#fef0d9',
+        BIN_2: '#fdcc8a',
+        BIN_3: '#fc8d59',
+        BIN_4: '#e34a33',
+        BIN_5: '#b30000'
+    },
     'EC': {
         center: [-32.2977935398105, 26.66272775514364],
         wardData: 'data/ECmerged.geojson',
+        wardDataTopoJson: 'data/ECmerged.topojson',
         shelterData: 'data/ECshelters.geojson',
         longName: 'Eastern Cape'
     },
@@ -53,28 +65,39 @@ ws.CONFIG = {
     'WC': {
         center: [-32.8, 20.168536735117264],
         wardData: 'data/WCmerged.geojson',
+        wardDataTopoJson: 'data/WCmerged.topojson',
         shelterData: 'data/WCshelters.geojson',
         longName: 'Western Cape'
     }
 }
-// end CONFIG
+// end CONFIG ******************************************
 
 // keep track of custom controls added to the map
 ws.customControls = [];
-ws.layers = {};
 // end keep track of custom controls added to the map
 
+// keep a reference to a layer placed on the map
+// ws.layer.name 
+ws.layers = {};
+// end keep a reference to a layer placed on the map
 
-// Event handlers registration ******************************
+
+// Document event handlers registration ******************************
+// executed when a json file has finished downloading from the server
 document.body.addEventListener('gotJsonRsrcOk',  (event) => {
-    if (event.detail.rsrcName === 'wardData') {
+    if (event.detail.rsrcName === 'wardDataGeoJson') {
         ws.wardLayer(event.detail.data);
     }
-    if (event.detail.rsrcName === 'shelterData') {
+    else if (event.detail.rsrcName === 'wardDataTopoJson') {
+        // handle topojson stuff !!!!!
+    }
+    else if (event.detail.rsrcName === 'shelterData') {
         ws.sheltersLayer(event.detail.data);
     }
 })
+// end
 
+// executed when user clicks on "Get Data" button ***********************************************
 document.getElementsByClassName("user-select__get-data")[0].addEventListener('click', (event) => {
     let autoTitle = (province, title) => {
         if (!title) {
@@ -83,9 +106,11 @@ document.getElementsByClassName("user-select__get-data")[0].addEventListener('cl
     }
     
     event.stopPropagation();
+    // read UI input
     let province = document.getElementsByClassName('user-select__select')[0].value;
     let title = document.getElementsByClassName('user-select__input')[0].value;
- 
+    // end read UI input
+
     // remove map layers.
     ws.map.eachLayer((layer) => {
         if (layer.name === 'wards' || layer.name === 'shelters') {
@@ -94,19 +119,23 @@ document.getElementsByClassName("user-select__get-data")[0].addEventListener('cl
     })
     // end remove map layers
 
-    // download correct data
-    ws.getJsonRsrc(ws.CONFIG[province].wardData, 'wardData');    
+    // download data
+    // geoJson map layer
+    ws.getJsonRsrc(ws.CONFIG[province].wardData, 'wardDataGeoJson'); 
+
+    // also want a topoJson layer !!!!
+    // pssoible also want a compressed topoJson layer !!!!
+
     ws.getJsonRsrc(ws.CONFIG.WC.shelterData, 'shelterData');
     ws.map.panTo(ws.CONFIG[province].center);
     // end download correct data
 
      // create legends
-     // !!!!!
     let col2legend = createLegend_c(autoTitle(province, title),
                 [{img: 'img/icons8-filled-circle-16.png', label: 'Shelters for woman'}]
             )
 
-    let col3legend = NEWcreateLegend_a('Female Population Density',
+    let col3legend = createLegend_a('Female Population Density',
         [   {color: '#fef0d9', low: '0', high: '2,000'},
             {color: '#fdcc8a', low: '2,000', high: '4,000'},
             {color: '#fc8d59', low: '4,000', high: '6,000'},
@@ -128,38 +157,43 @@ document.getElementsByClassName("user-select__get-data")[0].addEventListener('cl
     ws.customControls.push(createLegend('bottomright', [col2legend]));
     // end add legends to map
 })
+// end executed when user clicks on "Get Data" button ***************************************
 
+
+// executed when user clicks on "Map zoom toggle" button
 document.getElementsByClassName('user-select__toggle-zoom')[0].addEventListener('click', (event) => {
     // toggle zoom control
     // check if zoomControl on map
     if (ws.map.zoomControl._map) {
         ws.map.zoomControl.remove()
     } else {ws.map.zoomControl.addTo(ws.map)}
-    
-
 })
-// end Event handlers registration **************************
+// end
+// end document event handlers registration ************************************************
 
 
 // Custom Functions *****************************************
-let homeIcon = L.icon({
-    iconUrl: 'img/icons8-filled-circle-16.png',
-    iconSize:     [16, 16], // size of the icon
-    iconAnchor:   [8, 8], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
-});
 
+
+// give a layer a name so we can easily reference it later
 ws.nameLayer = (layerObj, layerName) => {
-    // give a layer a name
     if (layerObj.hasOwnProperty('name')) {
         console.log('cannot add "name" to layer')
     } else {layerObj.name = layerName;}
 }
+// end give a layer a name so we can easily reference it later
 
-
-ws.sheltersLayer = (layer) => {
-    ws.NEWsheltersLayer(layer)
-    return;
+// NOT USED
+// use an image instead of an SVG
+// only kept as an example  
+ws.OLDsheltersLayer = (layer) => {
+    let homeIcon = L.icon({
+        iconUrl: 'img/icons8-filled-circle-16.png',
+        iconSize:     [16, 16], // size of the icon
+        iconAnchor:   [8, 8], // point of the icon which will correspond to marker's location
+        popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    });
+   
     L.geoJSON(layer,  {
         pointToLayer: function(feature, latlng) {
             return L.marker(latlng, {icon: homeIcon}).bindPopup(feature.properties.name)
@@ -167,8 +201,10 @@ ws.sheltersLayer = (layer) => {
     })
     .addTo(ws.map);
 }
+// end NOT USED
 
-ws.NEWsheltersLayer = (layer) => {
+// create and add shelters layer
+ws.sheltersLayer = (layer) => {
     layerObj = L.geoJSON(layer, {
         pointToLayer: function(feature, latlng) {
                         return L.circleMarker(latlng, {
@@ -181,48 +217,57 @@ ws.NEWsheltersLayer = (layer) => {
                         }).bindPopup(feature.properties.name); 
         }
     })
+    // name  the layer
     ws.nameLayer(layerObj, 'shelters');
+    // add layer to map
     layerObj.addTo(ws.map);
+    // keep reference to layer in ws.layers.shelters
     ws.layers.shelters = layerObj;
 }
+// end 
 
 
-
-
+// create and add wardLayer from geoJson data
 ws.wardLayer = (layer) => {
     // layer argument: geoJSON data as json
     layer = L.geoJSON(layer, {style: ws.styleFeature})
     // layer: becomes a layer object
+    
+    // name the layer
     ws.nameLayer(layer, 'wards')
+    // add layer to map
     layer.addTo(ws.map);
 
-    // Event timing issue
+    // Event timing issue NOTE!
     ws.layers.shelters.bringToFront();
     ws.map.on('click', ws.onMapClick);
     // end Event timing issue
 }
 
-ws.getFillColor = (n) => {
-    // n --> no of woman in ward
-    BIN_SIZE = 2000
-    if (n > BIN_SIZE * 4) {return '#b30000';}
-    else if (n > BIN_SIZE * 3) {return '#e34a33';}
-    else if (n > BIN_SIZE * 2) {return '#fc8d59'; }
-    else if (n > BIN_SIZE * 1) {return '#fdcc8a'}
-    else {return '#fef0d9'}
-}
 
 
+// used to style ward areas with fill color based on female population size
 ws.styleFeature = (feature) => {
+
+    let getFillColor = (n) => {
+        // n --> no of woman in ward
+        let BIN_SIZE = ws.CONFIG.BIN_SIZE;
+        if (n > BIN_SIZE * 4) {return ws.CONFIG.COLORS.BIN_5;}
+        else if (n > BIN_SIZE * 3) {return ws.CONFIG.COLORS.BIN_4;}
+        else if (n > BIN_SIZE * 2) {return ws.CONFIG.COLORS.BIN_3; }
+        else if (n > BIN_SIZE * 1) {return ws.CONFIG.COLORS.BIN_2}
+        else {return ws.CONFIG.COLORS.BIN_1}
+    }
+
     return {
-        fillColor: ws.getFillColor(feature.properties.females),
+        fillColor: getFillColor(feature.properties.females),
         fillOpacity: .7,
         weight: 1,
         opacity: .8,
         color: 'lighgray',
     }
 }
-
+// end
 
 ws.onMapClick = (e) => {
     console.log('Map clicked at ',  e.latlng);
@@ -295,10 +340,15 @@ let addMapTitle = (title) => {
 }
 
 
+// legend(_b) of the form 
+//  title
+//  img     label
+//  img     label
 let createLegend_b = (title, rows) => {
     /*  rows: array of row objects
         row:{img: imgUrl, label: string}
         returns html string
+
     */
 
    let heading = `  <div class="legend-b__heading">
@@ -323,7 +373,12 @@ let createLegend_b = (title, rows) => {
     // add legend_b div wrapper
     return `<div class="legend-b">` + heading + rowsAll + `</div>`
 }
+// end
 
+// legend(_c) of the form 
+//  title
+//  circle     label
+//  circle     label
 let createLegend_c = (title, rows) => {
     /*  rows: array of row objects
         row:{label: string}
@@ -352,13 +407,13 @@ let createLegend_c = (title, rows) => {
     // add legend_c div wrapper
     return `<div class="legend-c">` + heading + rowsAll + `</div>`
 }
+// end
 
 
-
-
-
-
-let NEWcreateLegend_a = (title, rows, sep=' - ') => {
+// legend(_c) of the form 
+//  title
+//  swatch  low <sep> high
+let createLegend_a = (title, rows, sep=' - ') => {
     /*  title: text string
         rows: array of row objects {color: '#b30000', low: '0', high: '1,000'}
         sep:  seperator, text string
@@ -388,49 +443,11 @@ let NEWcreateLegend_a = (title, rows, sep=' - ') => {
     // return heading + rowsAll;
     return heading + rowsAll;
 }
+// end
 
 
 
 
-
-let createLegend_a = (title, rows, sep=' - ') => {
-    /*  title: text string
-        rows: array of row objects {color: '#b30000', low: '0', high: '1,000'}
-        sep:  seperator, text string
-    */
-
-   let legend = L.control({position: 'bottomright'});
-
-   legend.onAdd = function (map) {
-        ref.innerHTML = htmlToAdd;
-        return ref;
-    }        
-    
-    let ref = L.DomUtil.create('div', 'legend-a');
-    let rowsHtml = [];
-    let heading = ` <div class="legend-a__heading">
-                        <h4 class="u-center-text">${title}</h4>
-                    </div>`
-
-    for (let each of rows) {
-        let each_row =  `<div class="legend-a__row">
-                            <div class="legend-a__item-swatch" style="background-color: ${each.color};"></div>
-                            <div class="legend-a__item-low">${each.low}</div>
-                            <div class="legend-a__item-sep">${sep}</div>    
-                            <div class="legend-a__item-high">${each.high}</div>    
-                        </div>`
-        rowsHtml.push(each_row);
-    }
-    // assemble rowsHtml
-    let rowsAll = ``;
-    for (let each of rowsHtml) {
-        rowsAll = rowsAll + each
-    }
-
-    // return heading + rowsAll;
-    let htmlToAdd = heading + rowsAll;
-    legend.addTo(ws.map)
-}
 // end Map Components ***************************************
 
 
